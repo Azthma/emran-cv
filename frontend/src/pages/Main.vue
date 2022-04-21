734e0370d4d0fbc44300e9518f0599dac3f30f20
Emran <emranpendaliday1010@gmail.com>
2022-04-21 15:45:14 +0800

    justify

---
+++ frontend/src/pages/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-row class="grey darken-3" no-gutters>
+                <v-col cols="12" md="6" class="d-flex justify-center justify-lg-end align-center pr-lg-5 pt-5 pb-5" style="border: 1px solid black">
+                    <h1>Col One</h1>
+                </v-col>
+                <v-col cols="12" md="6" class="d-flex justify-center justify-lg-start align-center pl-lg-5 pt-5 pb-5" style="border: 1px solid black">
+                    <h1>Col Two</h1>
+                </v-col>
+            </v-row>
+        </v-main>
+    </v-app>
+</template>
+<style>
